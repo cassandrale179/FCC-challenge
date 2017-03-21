02daa01b08fdd9b8011c42c44744f60f8f53ecc5
Minh Le <lenguyetminh1998@gmail.com>
2017-03-21 10:09:37 -0400

    Update smallestmultiple.js

--- smallestmultiple.js
+++ smallestmultiple.js
@@ -1,4 +1,4 @@
-/*Problems: Find the smallest number X that is divisible of all number within given sorted array */     
+/*Find the smallest number X that is divisible of all number within given sorted array */     
 var array = [1,2,3,4,5,6,7] 
     var n = array[array.length-1]; 
     var i = 0; 
