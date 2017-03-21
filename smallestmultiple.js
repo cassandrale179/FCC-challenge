c24588b801414342dae76abd3e31b18c46615fb9
Minh Le <lenguyetminh1998@gmail.com>
2017-03-21 10:08:15 -0400

    Issue: infinite loop

--- smallestmultiple.js
+++ smallestmultiple.js
@@ -1,4 +1,5 @@
-    var array = [1,2,3,4,5,6,7] 
+/*Problems: Find the smallest number X that is divisible of all number within given sorted array */     
+var array = [1,2,3,4,5,6,7] 
     var n = array[array.length-1]; 
     var i = 0; 
     do{
@@ -10,4 +11,4 @@
     }while(i < array.length); 
     console.log(n);  
 
-
+/*Issues: test case that require massive iteration can result in infinite loop */ 
