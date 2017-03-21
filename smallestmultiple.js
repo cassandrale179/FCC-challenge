c425bb9512c763683076c4409edd6fb43e3969ab
Minh Le <lenguyetminh1998@gmail.com>
2017-03-20 22:56:16 -0400

    Update smallestmultiple.js

--- smallestmultiple.js
+++ smallestmultiple.js
@@ -1,18 +1,13 @@
-var array = [1,2,3,4,5,6,7,8,9,10,11,12,13]; 
-var n = array.length; 
-var i = 0; 
-do{
-    
-    var s = n % array[i]; 
-    if (s !== 0){
-        n++; 
-        i = 0; 
-    }
-    i++; 
-}while(i < array.length); 
-console.log(n); 
-
-
-
+    var array = [1,2,3,4,5,6,7] 
+    var n = array[array.length-1]; 
+    var i = 0; 
+    do{
+        if (n % array[i] !== 0){
+            n++; 
+            i = -1; 
+        }
+        i++; 
+    }while(i < array.length); 
+    console.log(n);  
 
 
