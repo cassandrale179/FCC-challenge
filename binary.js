92072f9cc17f0eb28e94535073d94f7c134e4a67
Minh Le <lenguyetminh1998@gmail.com>
2017-03-25 15:10:02 -0400

    Create an array to store ascii value

--- binary.js
+++ binary.js
@@ -1,9 +1,17 @@
+
 function binaryAgent(str) {
-  var num = "100101"; 
+  var ascii = []; 
+  var words = str.split(" "); 
   var sum = 0; 
-  for (var i = 0; i < num.length; i++){
-    sum = sum*2 + parseInt(num[i]); 
+  for (var i = 0; i < words.length; i++){
+    for (var j = 0; j < words[i].length; j++){
+      sum = sum*2 + parseInt(words[i][j]); 
+    }
+    ascii.push(sum); 
+    sum = 0; 
   }
-  return sum; 
+  return ascii; 
 }
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
  
