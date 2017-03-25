8630c483a619178e9c7ded1cdfed60c31d827366
Minh Le <lenguyetminh1998@gmail.com>
2017-03-25 19:32:07 -0400

    Create flattenarray.js

---
+++ flattenarray.js
@@ -0,0 +1,22 @@
+/*Flatten a nested array. You must account for varying levels of nesting.*/ 
+
+function steamrollArray(arr) {
+    var text = arr.toString(); 
+    text = text.replace(/,/g, ''); 
+    text = text.replace("[object Object]", "{"); 
+    var arr2 = []; 
+    for (var i = 0; i < text.length; i++){
+        if (text.charCodeAt(i) >= 48 && text.charCodeAt(i) <= 57){
+            arr2.push(parseInt(text[i])); 
+        }
+        else if (text.charCodeAt(i) == 123){
+            arr2.push({}); 
+        }
+        else{
+            arr2.push(text[i]); 
+        }
+    
+    }
+    return arr2; 
+}   
+    
