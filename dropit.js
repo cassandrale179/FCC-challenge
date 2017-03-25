5e37281396ffcfadea19ad78299c99f70832eb4a
Minh Le <lenguyetminh1998@gmail.com>
2017-03-25 13:21:06 -0400

    Update dropit.js

--- dropit.js
+++ dropit.js
@@ -1,15 +1,21 @@
 /* Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true. */ 
 function dropElements(arr, func) 
 {
-  var arr2 = []; 
-  for (var i = 0; i < arr.length; i++)
-  {
+  for (var i = 0; i < arr.length; i++){
+    if (func(arr[i]) === false){
+      arr.splice(i,1); 
+      i -= 1; 
+    }
     if (func(arr[i]) === true){
-     arr2.push(arr[i]); 
-      
+      break; 
     }
   }
-  return arr2; 
+  return arr; 
 }
 
-dropElements([1, 2, 3, 4], function(n) {return n >= 3;}); 
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}); 
+
+/*Test case: 
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
+*/ 
